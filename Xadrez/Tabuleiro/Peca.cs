d01d06e7e35d82f34b812cd3358d334b0317e01c
Gabriel Alves <gabi.12.spin@hotmail.com>
2018-10-23 13:39:31 -0300

    Criadas as Classes: Cor, Peca, Tabuleiro e iniciada a implementação do Tabuleiro

---
+++ Xadrez/Tabuleiro/Peca.cs
@@ -0,0 +1,17 @@
+﻿
+namespace tabuleiro {
+    class Peca {
+
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qtdeMovimentos { get; protected set; }
+        public Tabuleiro tabuleiro { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro) {
+            this.posicao = posicao;
+            this.cor = cor;
+            this.tabuleiro = tabuleiro;
+            qtdeMovimentos = 0;
+        }
+    }
+}
