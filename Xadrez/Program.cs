04f6b0116b379939dfb5be4ca43e346a451ab6db
Gabriel Alves <gabi.12.spin@hotmail.com>
2018-10-23 12:58:17 -0300

    Criada Classe Posicao

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Tabuleiro;
 
 namespace Xadrez {
     class Program {
         static void Main(string[] args) {
+
+            Posicao P;
+
+            P = new Posicao(3, 4);
         }
     }
 }
